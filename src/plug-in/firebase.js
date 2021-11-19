f7b003280a58252fa2712c4beed31e17e34203a8
TmoCdoot <timeopuse@gmail.com>
2021-11-19 22:01:27 +0100

    Initial commit

---
+++ src/plug-in/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore , doc, setDoc  } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut  } from "firebase/auth";
+
+const firebaseConfig = initializeApp({
+  apiKey: "AIzaSyAlQQBc9NSGNecaPtOMnAMwVPwvCBQM4oA",
+  authDomain: "cryptofuture41700.firebaseapp.com",
+  projectId: "cryptofuture41700",
+  storageBucket: "cryptofuture41700.appspot.com",
+  messagingSenderId: "443389965883",
+  appId: "1:443389965883:web:3e87159043bb2dc134ce1e"
+});
+
+const db = getFirestore(firebaseConfig);
+const auth = getAuth(firebaseConfig);
+
+export { db, doc, setDoc, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut };
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="box1">
+        <img alt="Logo" src="../assets/logo.png" />
+    </div>
+    <div class="widget">
+        <!-- composent 1 pour depot -->
+        <WidgetCustom1 title="Deposit" ammout="200"/>
+        <WidgetCustom1 title="Gain / Lost" ammout="124" signe="+" color1="#e67429" color2="#f3422b"/>
+        <!-- composent 2 pour gain/perte -->
+        <button @click="dz">fe</button>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+import WidgetCustom1 from "@/components/WidgetCustom1.vue";
+import { mapState } from 'vuex'
+import { auth, signOut } from '../plug-in/firebase.js';
+
+
+export default {
+  name: "Home",
+  components: {
+    WidgetCustom1,
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+  methods: {
+    dz: function () {
+      signOut(auth);
+      this.$router.push('/')
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.box1 {
+    border: 1px solid green;
+}
+
+.widget {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 10px;
+}
+</style>
